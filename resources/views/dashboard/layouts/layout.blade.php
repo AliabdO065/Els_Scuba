6771a5a3aff3a92eb746ee2f055fbbe66fcf0d35
Ali Abdo <aliabdoali421@gmail.com>
2025-09-12 15:42:21 +0300

    icons

--- resources/views/dashboard/layouts/layout.blade.php
+++ resources/views/dashboard/layouts/layout.blade.php
@@ -44,7 +44,7 @@
     <link rel="stylesheet" href="{{asset('dashboard/assets/stylesheets/theme.min.css')}}" data-skin="default">
     <link rel="stylesheet" href="{{asset('dashboard/assets/stylesheets/theme-dark.min.css')}}" data-skin="dark">
     <link rel="stylesheet" href="{{asset('dashboard/assets/stylesheets/custom.css')}}">
-    <link rel="stylesheet" href="{{asset('dashboard/assets/css/all.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     <script>
       var skin = localStorage.getItem('skin') || 'default';
